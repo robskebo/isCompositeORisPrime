281684891f1a9768cdfa88b9fb812206f073a9a3
Rob Skebo <robskebo@hotmail.com>
2019-05-14 22:22:50 -0400

    .
    .

--- isCompositeORisPrime.cpp
+++ isCompositeORisPrime.cpp
@@ -1,33 +1,47 @@
+// isCompisiteOrisPrime.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 // isCompositeORisPrime.cpp : Defines the entry point for the console application.
 
-#include "stdafx.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "ctype.h"
+#include <iostream>
 
-int main() 
+using namespace std;
+
+int main()
 {
 
-  int n, i, c = 0;
+	int n = 1, i = 0, c = 0;
 
-  printf("Enter any number n:");
-  scanf("%d", &n);
+	printf("Enter any number n:");
+	//scanf("%d", &n);
+	cin >> n;
 
-  for (i = 1; i <= n; i++) 
-  {
-      if (n % i == 0) 
-	  {
-         c++;
-      }
-  }
-  if (c == 2) 
-  {
-  	printf("n is a Prime number");
-	printf("\n");
-  }
-  else {
- 	 printf("n is a Composite number");
-	 printf("\n");
-  }
-  return 0;    
-}  
+	for (i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			c++;
+		}
+	}
+	if (c == 2)
+	{
+		cout << "n is a Prime number";
+		cout << endl;
+	}
+	else {
+			cout << "n is a Composite number";
+			cout << endl;
+		 }
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
